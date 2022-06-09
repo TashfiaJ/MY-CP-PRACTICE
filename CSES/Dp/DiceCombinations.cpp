83f4b88947aa5f44fa4cec41879f8cc5675813b4
Tashfia Jannath <76718979+TashfiaJ@users.noreply.github.com>
2022-06-10 04:46:54 +0600

    Update DiceCombinations.cpp

--- CSES/Dp/DiceCombinations.cpp
+++ CSES/Dp/DiceCombinations.cpp
@@ -1,1 +1,25 @@
-hehe
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+ 
+ll dp[1000005];
+ 
+ll dice(ll sum)
+{
+    if(sum==0)return 1;
+    if(dp[sum]!=-1)return dp[sum];
+    ll way=0;
+    for(ll j=1;j<7;j++){
+        if(sum-j>=0)
+    way += dice(sum-j);}
+ 
+    return dp[sum]=(way%1000000007);
+}
+ 
+int main()
+{
+    
+    memset(dp, -1, sizeof(dp));
+    ll sum; cin >> sum;
+    cout << dice(sum);
+}
