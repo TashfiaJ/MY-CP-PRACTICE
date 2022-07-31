7725f33512794382002bfeabe1886119edc17585
Tashfia Jannath <76718979+TashfiaJ@users.noreply.github.com>
2022-07-31 23:44:26 +0600

    Update Prime_factorization_of_factorial.cpp

--- templates/Number Theory/Prime_factorization_of_factorial.cpp
+++ templates/Number Theory/Prime_factorization_of_factorial.cpp
@@ -6,21 +6,20 @@ vector<ll>primes;
 void SieveOfEratosthenes()
 {
     
-    for(ll i=0;i<N;i++)
-        prime[i]=1;
+    memset(prime, 1, sizeof(prime));
 
     for(ll i = 4; i<N; i+=2)
         prime[i]=0;
 
     for (ll p = 3; p*p <N ; p+=2) {
         
-        if (prime[p] == 1) {
+        if (prime[p]) {
             for (ll i = p * p; i<N; i += p)
                 prime[i] = 0;
         }
     }
     for (ll p = 2; p<N; p++){
-        if (prime[p]==1)
+        if (prime[p])
             primes.push_back(p);}
 }
 
