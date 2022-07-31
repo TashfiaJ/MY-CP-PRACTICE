aead62b6e5895056e18595d1a87443e57191178d
Tashfia Jannath <76718979+TashfiaJ@users.noreply.github.com>
2022-07-31 23:43:36 +0600

    Create Intelligent_Factorial_Factorization.cpp

---
+++ LightOJ/Math/Intelligent_Factorial_Factorization.cpp
@@ -0,0 +1,62 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+
+const ll N  = 105;
+
+ll prime[N];
+vector<ll>primes;
+
+void SieveOfEratosthenes()
+{
+    
+    memset(prime, 1, sizeof(prime));
+
+    for(ll i = 4; i<N; i+=2)
+        prime[i]=0;
+
+    for (ll p = 3; p*p <N ; p+=2) {
+        
+        if (prime[p]) {
+            for (ll i = p * p; i<N; i += p)
+                prime[i] = 0;
+        }
+    }
+    for (ll p = 2; p<N; p++){
+        if (prime[p])
+            primes.push_back(p);}
+}
+
+void factFactorize (ll n) {
+   
+    for (ll i = 0; i<N && primes[i] <= n; i++ ) {
+        ll x = n;
+        ll freq = 0;
+ 
+        while ( x / primes[i] ) {
+            freq += x / primes[i];
+            x = x / primes[i];
+        }
+ 
+        
+        cout << primes[i] << " (" << freq << ")";
+        if(n/primes[i+1])cout << " * ";
+    }
+}
+
+int main()
+{   
+    
+    SieveOfEratosthenes();
+    ll tc;
+    cin >> tc;
+    ll t=1;
+    while(tc--)
+    {
+        ll n;
+        cin >> n;
+        cout <<"Case " << t++ << ": " << n << " = "; 
+        factFactorize(n);
+        cout << '\n';
+    }
+}
