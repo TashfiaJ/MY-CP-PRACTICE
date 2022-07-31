717f775fa4a80c51f301bc41d0823875f975f805
Tashfia Jannath <76718979+TashfiaJ@users.noreply.github.com>
2022-07-31 23:41:38 +0600

    Create Prime_factorization_of_factorial.cpp

---
+++ templates/Number Theory/Prime_factorization_of_factorial.cpp
@@ -0,0 +1,42 @@
+const ll N  = 105;
+
+ll prime[N];
+vector<ll>primes;
+
+void SieveOfEratosthenes()
+{
+    
+    for(ll i=0;i<N;i++)
+        prime[i]=1;
+
+    for(ll i = 4; i<N; i+=2)
+        prime[i]=0;
+
+    for (ll p = 3; p*p <N ; p+=2) {
+        
+        if (prime[p] == 1) {
+            for (ll i = p * p; i<N; i += p)
+                prime[i] = 0;
+        }
+    }
+    for (ll p = 2; p<N; p++){
+        if (prime[p]==1)
+            primes.push_back(p);}
+}
+
+void factFactorize (ll n) {
+   
+    for (ll i = 0; i<N && primes[i] <= n; i++ ) {
+        ll x = n;
+        ll freq = 0;
+ 
+        while ( x / primes[i] ) {
+            freq += x / primes[i];
+            x = x / primes[i];
+        }
+ 
+        
+        cout << primes[i] << " (" << freq << ")";
+        if(n/primes[i+1])cout << " * ";
+    }
+}
