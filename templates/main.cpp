e28177de4a08252707c5740f50ff4a2f239e5757
Tashfia Jannath <76718979+TashfiaJ@users.noreply.github.com>
2022-08-27 19:11:51 +0600

    Create main.cpp

---
+++ templates/main.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long int ll;
+typedef pair<ll, ll> pi;
+#define mp make_pair
+#define pb push_back
+#define F first
+#define S second
+
+void solve()
+{
+    
+}
+
+int main()
+{
+#ifndef ONLINE_JUDGE
+freopen("input.txt", "r", stdin);
+#else
+#endif
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);cout.tie(NULL);
+    ll T; T=1; 
+    cin >> T;
+    while(T--)
+    {
+        solve();
+    }
+}
