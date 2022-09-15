7eb7c6ff9f9749655a741332342ef7dfa9bf8afb
Tashfia Jannath <76718979+TashfiaJ@users.noreply.github.com>
2022-09-15 23:06:09 +0600

    Update main.cpp

--- templates/main.cpp
+++ templates/main.cpp
@@ -14,10 +14,7 @@ void solve()
 
 int main()
 {
-#ifndef ONLINE_JUDGE
-freopen("input.txt", "r", stdin);
-#else
-#endif
+    freopen("input.txt", "r", stdin);
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);cout.tie(NULL);
     ll T; T=1; 
