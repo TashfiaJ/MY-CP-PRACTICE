4bd2d09a72d3812b73a23867eb4c20f361dda16c
Tashfia Jannath <76718979+TashfiaJ@users.noreply.github.com>
2022-10-27 00:25:40 +0600

    Create Magic Numbers.cpp

---
+++ CodeMarshal/Battle_of_Brains 2020/Magic Numbers.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long int ll;
+typedef pair<ll, ll> pi;
+#define mp make_pair
+#define pb push_back
+#define F first
+#define S second
+
+void solve()
+{
+    ll m, k;
+    cin >> m >> k;
+    cout << m%10;
+}
+
+int main()
+{
+    //freopen("input.txt", "r", stdin);
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);cout.tie(NULL);
+    ll T; T=1;
+    //cin >> T;
+    while(T--)
+    {
+        solve();
+    }
+}
