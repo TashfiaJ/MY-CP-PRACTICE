03b357cac75e1fdb3e7231d95d9bcedb8264232c
Tashfia Jannath <76718979+TashfiaJ@users.noreply.github.com>
2022-11-02 14:00:30 +0600

    Create Calkin_Wilf_Tree.cpp

---
+++ templates/Number Theory/Calkin_Wilf_Tree.cpp
@@ -0,0 +1,23 @@
+// Given (a/b), find the height 
+// it can be solved using euclidian algo
+
+void cwt()
+{
+    ll a, b;
+    cin >> a >> b;
+    ll height=0;
+    while(a&&b)
+    {
+        if(a>=b)
+        {   
+           height+=(a/b);
+           a%=b;
+        }
+        else
+        {    
+            height+=(b/a);
+            b%=a;
+        }
+    }
+    cout << height;
+}
