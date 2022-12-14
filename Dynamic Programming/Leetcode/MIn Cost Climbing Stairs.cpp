a011f84ff0b4c785afcc46f17d33f1938f4ad931
Tashfia Jannath <76718979+TashfiaJ@users.noreply.github.com>
2022-12-14 20:12:33 +0600

    Update MIn Cost Climbing Stairs.cpp

--- Dynamic Programming/Leetcode/MIn Cost Climbing Stairs.cpp
+++ Dynamic Programming/Leetcode/MIn Cost Climbing Stairs.cpp
@@ -21,3 +21,29 @@ public:
         return solve(cost.size(), dp, cost);
     }
 };
+
+// Tabulation
+// time complexity = O(n)
+// space complexity = O(n)+O(n)
+class Solution {
+public:
+    int solve(int ind, int dp[], vector<int>& cost)
+    {
+        dp[0]=0;
+        dp[1]=0;
+        for(int i=2;i<=ind;i++)
+        {
+            dp[i]=dp[i-1]+cost[i-1];
+            if(i>1)dp[i]=min(dp[i], dp[i-2]+cost[i-2]);
+        }
+        return dp[ind];
+    }
+    int minCostClimbingStairs(vector<int>& cost) {
+        int dp[cost.size()+1];
+        for(int i=0;i<=cost.size();i++)
+        {
+            dp[i]=-1;
+        }
+        return solve(cost.size(), dp, cost);
+    }
+};
