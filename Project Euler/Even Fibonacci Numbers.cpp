82d3aee267cc40c29cd969e36f27139e8f5549eb
Tashfia Jannath <76718979+TashfiaJ@users.noreply.github.com>
2023-07-31 13:19:57 +0600

    Create Even Fibonacci Numbers.cpp

---
+++ Project Euler/Even Fibonacci Numbers.cpp
@@ -0,0 +1,61 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long int ll;
+typedef pair<ll, ll> pi;
+#define lll __int128_t
+#define mp make_pair
+#define pb push_back
+#define F first
+#define S second
+#define SetBit(x, k) (x |= (1LL << k))
+#define ClearBit(x, k) (x &= ~(1LL << k))
+#define CheckBit(x, k) (x & (1LL << k))
+const ll inf = 1LL<<60; //1.15e18
+const ll md = 1000000007;
+
+ll dx[]={0,1,0,-1};
+ll dy[]={1,0,-1,0};
+ll dxx[]={0,1,0,-1,1,1,-1,-1};
+ll dyy[]={1,0,-1,0,1,-1,1,-1};
+
+mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+int my_random(int l, int r)
+{
+    return uniform_int_distribution<int>(l, r) (rng);
+}
+
+
+
+void solve()
+{   
+    ll N;
+    cin >> N;
+    ll pre1=1, pre2=2, s=0, n=2;
+    while(n<=N)
+    {
+        s+=n;
+        for(ll i=1;i<=3;i++)
+        {
+            n=pre1+pre2;
+            pre1=pre2;
+            pre2=n;
+        }
+    }
+    cout<<s<<'\n';
+}
+
+int main()
+{
+    //freopen("input.txt", "r", stdin);
+    //freopen("output.txt", "w", stdout);
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);cout.tie(NULL);
+    ll T; T=1; 
+    cin >> T;
+    while(T--)
+    {
+        solve();
+    }
+    return 0;
+}
+
